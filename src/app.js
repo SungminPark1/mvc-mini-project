292b1919f9dcf49df421743647b20b0635e1f56f
Sungmin Park <sp4216@rit.edu>
2016-04-12 12:12:36 -0400

    attempt to fix errors

--- src/app.js
+++ src/app.js
@@ -77,8 +77,8 @@ app.use(function(err, req, res, next){
 
 router(app);
 
-//var server = require('http').createServer(app);
-//var io = socketio(server);
+var server = require('http').createServer(app);
+var io = socketio(server);
 
 app.listen(port, function(err){
 	if(err){
@@ -87,7 +87,7 @@ app.listen(port, function(err){
 	console.log('Listen on port ' + port);
 });
 
-/*
+
 io.sockets.on("connection", function(socket) {
 	ioServer.onJoined(socket);
 	ioServer.onMsg(socket);
@@ -97,5 +97,5 @@ io.sockets.on("connection", function(socket) {
 setInterval(function(){
 	ioServer.update(io);
 }, 1000/60);
-*/
+
 console.log('websocket server started');
